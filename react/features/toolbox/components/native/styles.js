a77d9aaad87710e030ea6e94c4d150f00f335417
MAshar <ashar@claryicon.com>
2021-10-06 23:41:00 +0500

    Removed check from display name and changed style of toolbox.

--- react/features/toolbox/components/native/styles.js
+++ react/features/toolbox/components/native/styles.js
@@ -82,14 +82,14 @@ const styles = {
      */
     toolbox: {
         alignItems: 'center',
-        backgroundColor: ColorPalette.darkBackground,
+        backgroundColor: 'transparent',
         borderTopLeftRadius: 3,
         borderTopRightRadius: 3,
         flexDirection: 'row',
         flexGrow: 0,
         justifyContent: 'space-between',
         paddingHorizontal: BoxModel.margin,
-        paddingVertical: 8
+        paddingVertical: 20
     },
 
     /**
