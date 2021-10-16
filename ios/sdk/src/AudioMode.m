80a83e72324e34d053c037b782bb76d09e179bcd
MAshar <ashar@claryicon.com>
2021-10-16 11:15:23 +0500

    Set earpiece for audio only call.

--- ios/sdk/src/AudioMode.m
+++ ios/sdk/src/AudioMode.m
@@ -294,7 +294,7 @@ RCT_EXPORT_METHOD(setInitialRouteToEarpiece:(BOOL) isInitialEarpiece){
 
     switch (mode) {
         case kAudioModeAudioCall:
-            return audioCallConfig;
+            return earpieceConfig;
         case kAudioModeDefault:
             return defaultConfig;
         case kAudioModeVideoCall:
