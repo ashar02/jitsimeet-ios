f75a3e0e4d21331f8749cb9e33e20740ab73cc25
Farhan Aslam <farhanaslam@192.168.10.11>
2022-03-07 11:43:13 +0500

    current user box vertical in case of 2 user on video call

--- react/features/filmstrip/components/native/LocalThumbnail.js
+++ react/features/filmstrip/components/native/LocalThumbnail.js
@@ -32,16 +32,16 @@ class LocalThumbnail extends Component<Props> {
         const styleOverrides = {
             aspectRatio: null,
             flex: 1,
-            height: 140,
-            maxHeight:  participantsCount == 3 ? 100 : participantsCount > 5 ? 100 : 140,
-            maxWidth: participantsCount == 3 ? 100 : participantsCount > 5 ? 100 : 140,
-            width: 140,
+            height: 440,
+            maxHeight:  participantsCount == 2 ? 400 : participantsCount == 3 ? 100 : participantsCount > 5 ? 100 : 140,
+            maxWidth: participantsCount == 2 ? 200 : participantsCount == 3 ? 100 : participantsCount > 5 ? 100 : 140,
+            width: 440,
             borderRadius:16,
             marginRight:10
         };
 
         return (
-            <View style = { styles.localThumbnail }>
+            <View style = {{aspectRatio: participantsCount == 2 ? 0.6 : 1}}>
                 <Thumbnail participant = { _localParticipant } 
                 styleOverrides={styleOverrides}
                 renderDisplayName = {participantsCount == 3 ? false : participantsCount > 5 ? false : true }
